c3d862d74483c46ae03445e2d9d74f18b9a720f0
James Elderfield <jad.elderfield@gmail.com>
2021-12-11 14:53:12 +0200

    Add day 2 part 1 solution

--- src/main.rs
+++ src/main.rs
@@ -58,8 +58,78 @@ fn day_one() -> (i32, i32) {
     return (number_of_increases, number_of_summed_increases);
 }
 
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+struct CourseInstruction {
+    direction: Direction,
+    distance: i32,
+}
+
+fn parse_instruction(input: &String) -> CourseInstruction {
+    let parts: Vec<&str> = input.split(" ").collect();
+
+    if parts.len() != 2 {
+        panic!("Invalid direction {}", input);
+    }
+
+    let direction = match parts[0] {
+        "forward" => Direction::Forward,
+        "down" => Direction::Down,
+        "up" => Direction::Up,
+        _ => panic!("Invalid direction {}", input),
+    };
+
+    let distance = match i32::from_str(parts[1]) {
+        Ok(v) => v,
+        Err(_) => panic!("Invalid direction {}", input),
+    };
+
+    return CourseInstruction {
+        direction,
+        distance,
+    };
+}
+
+fn day_two() -> i32 {
+    let in_file_name = "inputs/ex2.txt";
+
+    let file = File::open(in_file_name);
+
+    if file.is_err() {
+        panic!("Could not open {}", in_file_name);
+    }
+
+    let mut challenge_input: Vec<String> = Vec::new();
+
+    for line in io::BufReader::new(file.unwrap()).lines() {
+        match line {
+            Ok(v) => challenge_input.push(v),
+            Err(_) => panic!("Failed to read line"),
+        }
+    }
+
+    let as_instructions: Vec<CourseInstruction> =
+        challenge_input.iter().map(parse_instruction).collect();
+
+    let final_position =
+        as_instructions
+            .iter()
+            .fold((0, 0), |(x, y), instruction| match instruction.direction {
+                Direction::Forward => (x + instruction.distance, y),
+                Direction::Down => (x, y + instruction.distance),
+                Direction::Up => (x, y - instruction.distance),
+            });
+
+    return final_position.0 * final_position.1;
+}
+
 fn main() {
     println!("Hello, world!");
 
     println!("Day one solution: {:?}", day_one());
+    println!("Day two solution: {:?}", day_two());
 }

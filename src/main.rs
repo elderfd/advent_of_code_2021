80b83e6ee81ebb1707adfd581919e6fc75a15935
James Elderfield <jad.elderfield@gmail.com>
2021-12-11 15:04:56 +0200

    Remove hello world

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod day_one;
 mod day_two;
 
 fn main() {
-    println!("Hello, world!");
-
     println!("Day one solution: {:?}", day_one::solve());
     println!("Day two solution: {:?}", day_two::solve());
 }

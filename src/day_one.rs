748ac98952fa3d42ca0f8de589dbed81604f89e6
James Elderfield <jad.elderfield@gmail.com>
2021-12-11 15:04:40 +0200

    Refactor to avoid giant files

---
+++ src/day_one.rs
@@ -0,0 +1,69 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+pub fn solve() -> (i32, i32) {
+    let in_file_name = "inputs/day_one.txt";
+
+    let file = File::open(in_file_name);
+
+    if file.is_err() {
+        panic!("Could not open {}", in_file_name);
+    }
+
+    let mut challenge_input: Vec<String> = Vec::new();
+
+    for line in io::BufReader::new(file.unwrap()).lines() {
+        match line {
+            Ok(v) => challenge_input.push(v),
+            Err(_) => panic!("Failed to read line"),
+        }
+    }
+
+    let input_numbers: Vec<i32> = challenge_input
+        .iter()
+        .map(|i| match i32::from_str(i) {
+            Ok(v) => v,
+            Err(_) => panic!("Invalid input"),
+        })
+        .collect();
+
+    let number_of_increases = input_numbers
+        .iter()
+        .fold((0, None), |acc, v| match acc {
+            (count, None) => (count, Some(v)),
+            (count, Some(last_v)) => (if last_v < v { count + 1 } else { count }, Some(v)),
+        })
+        .0;
+
+    let number_of_summed_increases = input_numbers
+        .windows(3)
+        .fold((0, None), |acc: (i32, Option<i32>), v: &[i32]| {
+            let triplet_sum = v.iter().sum();
+
+            match acc {
+                (count, None) => (count, Some(triplet_sum)),
+                (count, Some(last_v)) => (
+                    if last_v < triplet_sum {
+                        count + 1
+                    } else {
+                        count
+                    },
+                    Some(triplet_sum),
+                ),
+            }
+        })
+        .0;
+
+    return (number_of_increases, number_of_summed_increases);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_answer() {
+        assert_eq!(solve(), (1228, 1257))
+    }
+}
---
+++ src/day_two.rs
@@ -0,0 +1,100 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::str::FromStr;
+
+enum Direction {
+    Forward,
+    Down,
+    Up,
+}
+
+struct CourseInstruction {
+    direction: Direction,
+    distance: i32,
+}
+
+fn parse_instruction(input: &String) -> CourseInstruction {
+    let parts: Vec<&str> = input.split(" ").collect();
+
+    if parts.len() != 2 {
+        panic!("Invalid direction {}", input);
+    }
+
+    let direction = match parts[0] {
+        "forward" => Direction::Forward,
+        "down" => Direction::Down,
+        "up" => Direction::Up,
+        _ => panic!("Invalid direction {}", input),
+    };
+
+    let distance = match i32::from_str(parts[1]) {
+        Ok(v) => v,
+        Err(_) => panic!("Invalid direction {}", input),
+    };
+
+    return CourseInstruction {
+        direction,
+        distance,
+    };
+}
+
+pub fn solve() -> (i32, i32) {
+    let in_file_name = "inputs/day_two.txt";
+
+    let file = File::open(in_file_name);
+
+    if file.is_err() {
+        panic!("Could not open {}", in_file_name);
+    }
+
+    let mut challenge_input: Vec<String> = Vec::new();
+
+    for line in io::BufReader::new(file.unwrap()).lines() {
+        match line {
+            Ok(v) => challenge_input.push(v),
+            Err(_) => panic!("Failed to read line"),
+        }
+    }
+
+    let as_instructions: Vec<CourseInstruction> =
+        challenge_input.iter().map(parse_instruction).collect();
+
+    let simple_interpretation =
+        as_instructions
+            .iter()
+            .fold((0, 0), |(x, y), instruction| match instruction.direction {
+                Direction::Forward => (x + instruction.distance, y),
+                Direction::Down => (x, y + instruction.distance),
+                Direction::Up => (x, y - instruction.distance),
+            });
+
+    let complex_interpretation = as_instructions
+        .iter()
+        .fold(
+            ((0, 0), 0),
+            |((x, y), aim), instruction| match instruction.direction {
+                Direction::Forward => (
+                    (x + instruction.distance, y + aim * instruction.distance),
+                    aim,
+                ),
+                Direction::Down => ((x, y), aim + instruction.distance),
+                Direction::Up => ((x, y), aim - instruction.distance),
+            },
+        )
+        .0;
+
+    return (
+        simple_interpretation.0 * simple_interpretation.1,
+        complex_interpretation.0 * complex_interpretation.1,
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn correct_answer() {
+        assert_eq!(solve(), (1815044, 1739283308))
+    }
+}
